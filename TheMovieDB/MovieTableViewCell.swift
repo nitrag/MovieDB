8b195d642e6236cffcf97e047709cf6f8fc6f94b
Ryan Gartin <ryan@eagledevelopers.net>
2019-04-29 21:33:05 -0400

    Scaffolding UI - 30min

---
+++ TheMovieDB/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  TheMovieDB
+//
+//  Created by Ryan Gartin on 4/29/19.
+//  Copyright © 2019 Ryan Gartin. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
