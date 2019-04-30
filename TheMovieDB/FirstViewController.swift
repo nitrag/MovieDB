4809b88b9b8c1db867267f3384a53bdb43b64d03
Ryan Gartin <ryan@eagledevelopers.net>
2019-04-29 21:11:54 -0400

    Initial Commit

---
+++ TheMovieDB/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TheMovieDB
+//
+//  Created by Ryan Gartin on 4/29/19.
+//  Copyright © 2019 Ryan Gartin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TheMovieDB/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TheMovieDB
+//
+//  Created by Ryan Gartin on 4/29/19.
+//  Copyright © 2019 Ryan Gartin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
